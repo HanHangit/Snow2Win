666dd14cfae5b48f376bd1f0d104f9f90a2bf9d6
12Cy <christian.sandkaemper@gmail.com>
2016-12-16 19:20:05 +0100

    add new tiles

--- ScharlieAndSnow/Player/Player.cs
+++ ScharlieAndSnow/Player/Player.cs
@@ -10,13 +10,20 @@ using Microsoft.Xna.Framework.Input;
 
 namespace ScharlieAndSnow
 {
-    
+
+
+    public enum Direction { Up, Left, Right };
+    public enum State { Normal, Dying };
+
     class Player
     {
+        float speed;
+        Vector2 _mov;
+        Vector2 _pos;
         Texture2D playerTexture;
-        Vector2 startPosition;
+
         public Player(Vector2 _startPosition) {
-            startPosition = _startPosition;
+            _pos = _startPosition;
         }
         public void LoadContent(ContentManager content)
         {
@@ -24,10 +31,30 @@ namespace ScharlieAndSnow
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(playerTexture, startPosition);
+            spriteBatch.Draw(playerTexture, _pos);
         }
         public void Update(GameTime gTime)
         {
+            Keys[] pressedKeys = (from k in Keyboard.GetState().GetPressedKeys()
+                                  where PlayerManager.validKeys.Contains(k)
+                                  select k).ToArray();
+
+
+
+            if (pressedKeys.Length != 1)
+                return;
+            //Debug Shit
+            for (int i = 0; i < pressedKeys.Length; i++)
+                Console.WriteLine(pressedKeys[i].ToString());
+
+            _mov = Vector2.Zero;
+            //if (pressedKeys[0]) == PlayerManager.
+
+
+            //MapStuff.Instance.map.Walkable(); //position Walkable and CheckSnow
+            //MapStuff.Instance.map.CheckSnow(); Checkt ob dort Schnell liegt
+            //MapStuff.Instance.map.CollectSnow(); Sammelt Schnell auf (Funktion entfernt schnee und lässt nachrutschen)
+
 
         }
     }
