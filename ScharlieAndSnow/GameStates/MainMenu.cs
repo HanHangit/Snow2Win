cd9531912cd1081b16c63bc53768dd3f8a7cfdcd
12Cy <christian.sandkaemper@gmail.com>
2017-01-02 22:46:59 +0100

    LittleBird = littleFun jippi!

--- ScharlieAndSnow/GameStates/MainMenu.cs
+++ ScharlieAndSnow/GameStates/MainMenu.cs
@@ -28,11 +28,27 @@ namespace ScharlieAndSnow
         MyButton continueButton;
         MyButton exitButton2;
         Texture2D background;
+
+        #region testfieldBird
+        Texture2D birdTexture;
+        Rectangle destRect;
+        Rectangle sourceRect;
+
+        float elapsed;
+        float delay = 120f;
+        int frames = 0;
+
+        Vector2 pos;
+
+        #endregion
+
         String headline = "ScharlieAndSnow";
         bool troll;
         public MainMenu()
         {
             background = MyContentManager.GetTexture(MyContentManager.TextureName.BackGround_1270x720);
+
+            pos = new Vector2(-150, 15);
 
             troll = false;
             Console.WriteLine("MainMenu");
@@ -42,24 +58,13 @@ namespace ScharlieAndSnow
             exitButton = new MyButton("Exit Game", 200, 40, Constant.Calculate(40, 68), MyContentManager.GetFont(MyContentManager.FontName.Arial), Color.Red);
             shitClick = new MyButton("Keine Tasten drücken, sondern die Maus bedienen! Faule Studenten", 600, 40, Constant.Calculate(1, 80), MyContentManager.GetFont(MyContentManager.FontName.Arial), Color.Red);
 
+            birdTexture = MyContentManager.GetTexture(MyContentManager.TextureName.littleBird);
+            destRect = new Rectangle((int)pos.X, (int)pos.Y, 64, birdTexture.Height);
 
-
-    }
-
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(background, new Vector2(0, 0));
-            spriteBatch.DrawString(MyContentManager.GetFont(MyContentManager.FontName.Arial), headline, Constant.Calculate(25, 20), Color.Red, 0, new Vector2(0,0),3, SpriteEffects.None, 0);
-
-            continueButton.Draw(spriteBatch);
-            //playButton.Draw(spriteBatch);
-            exitButton2.Draw(spriteBatch);
-            //exitButton.Draw(spriteBatch);
-
-            if (troll)
-                shitClick.Draw(spriteBatch);
         }
+
+
+
 
         public void Initialize()
         {
@@ -78,6 +83,7 @@ namespace ScharlieAndSnow
 
         public EGameState Update(GameTime gTime)
         {
+            FlyLittleBird(gTime);
             continueButton.Update(gTime);
             playButton.Update(gTime);
             exitButton.Update(gTime);
@@ -99,6 +105,41 @@ namespace ScharlieAndSnow
                 troll = false;
 
             return EGameState.Mainmenu;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(background, new Vector2(0, 0));
+            spriteBatch.DrawString(MyContentManager.GetFont(MyContentManager.FontName.Arial), headline, Constant.Calculate(25, 20), Color.Red, 0, new Vector2(0, 0), 3, SpriteEffects.None, 0);
+            spriteBatch.Draw(birdTexture, destRect, sourceRect, Color.White);
+            continueButton.Draw(spriteBatch);
+            //playButton.Draw(spriteBatch);
+            exitButton2.Draw(spriteBatch);
+            //exitButton.Draw(spriteBatch);
+
+            if (troll)
+                shitClick.Draw(spriteBatch);
+        }
+
+
+        void FlyLittleBird(GameTime gTime)
+        {
+            if (pos.X > Constant.x)
+                pos.X = -150;
+
+            pos.X = pos.X + 1.8f;
+            destRect = new Rectangle((int)pos.X, (int)pos.Y, 128, birdTexture.Height);
+
+            if(elapsed >= delay)
+            {
+                if (frames >= 9)
+                    frames = 0;
+                else
+                    frames++;
+                elapsed = 0;
+            }
+            sourceRect = new Rectangle(birdTexture.Width / 10 * frames, 0, 128, 128);
+            elapsed += (float)gTime.ElapsedGameTime.TotalMilliseconds;
         }
     }
 }
